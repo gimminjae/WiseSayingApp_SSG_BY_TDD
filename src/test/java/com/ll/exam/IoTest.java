b268e4b7295f3b039d5d7c64f8e59cfa0e757bf0
gimminjae <min356812@naver.com>
2022-07-30 08:55:26 +0900

    TestUtil도입, assertJ도입, 테스트를 위한 입출력 리다이렉팅

---
+++ src/test/java/com/ll/exam/IoTest.java
@@ -0,0 +1,27 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IoTest {
+    @Test
+    public void 스캐너에_키보드가_아닌_문자열을_입력으로_설정() {
+        Scanner sc = TestUtil.genScanner("Hello");
+
+        String cmd = sc.nextLine().trim();
+        assertThat(cmd).isEqualTo("Hello");
+    }
+    @Test
+    public void 출력을_모니텨에_하지_않고_문자열로_얻기() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("Hello");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs).isEqualTo("Hello");
+    }
+}
