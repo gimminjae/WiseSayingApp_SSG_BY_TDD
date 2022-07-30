b0c215fae08191e5dbaddf8279068194256eb876
gimminjae <min356812@naver.com>
2022-07-30 10:25:41 +0900

    lombok도입

--- src/main/java/com/ll/exam/WiseSaying.java
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -1,37 +1,16 @@
 package com.ll.exam;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class WiseSaying {
     private int id;
     private String author;
     private String wiseSaying;
 
-    public WiseSaying(int id, String author, String wiseSaying) {
-        this.id = id;
-        this.author = author;
-        this.wiseSaying = wiseSaying;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getWiseSaying() {
-        return wiseSaying;
-    }
-
-    public void setWiseSaying(String wiseSaying) {
-        this.wiseSaying = wiseSaying;
-    }
 }
