99c1ea283f8a22f9bbf5b2010bff83e8de2edcd4
gimminjae <min356812@naver.com>
2022-07-30 09:30:13 +0900

    종료 기능 구현 및 테스트

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,6 +2,9 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -10,5 +13,18 @@ public class AppTest {
     public void Test() {
         assertTrue(1 == 1);
         assertEquals(1, 1);
+    }
+    @Test
+    void 프로그램_시작_및_종료_Test() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertTrue(rs.contains("== 명언 SSG =="));
+        assertTrue(rs.contains("명령) "));
     }
 }
