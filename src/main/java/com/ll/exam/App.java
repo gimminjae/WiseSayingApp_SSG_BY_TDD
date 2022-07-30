179ae73199be41b539347e6d89539fc83f17a450
gimminjae <min356812@naver.com>
2022-07-30 10:22:46 +0900

    명언 목록 기능 구현 및 테스트, 일부 테스트 수정

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,12 +1,14 @@
 package com.ll.exam;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
+    private List<WiseSaying> wsList;
     private Scanner sc;
     private int id;
 
     public App(Scanner sc) {
+        wsList = new LinkedList<>();
         this.sc = sc;
         id = 1;
     }
@@ -23,9 +25,22 @@ public class App {
             switch (cmd) {
                 case "등록":
                     System.out.print("명언 : ");
+                    String wiseSaying = sc.nextLine();
+
                     System.out.print("작가 : ");
+                    String author = sc.nextLine();
+
+                    wsList.add(new WiseSaying(id, author, wiseSaying));
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id++);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+
+                    for(int i = wsList.size() - 1; i >= 0; i--) {
+                        WiseSaying ws = wsList.get(i);
+                        System.out.printf("%d / %s / %s\n", ws.getId(), ws.getAuthor(), ws.getWiseSaying());
+                    }
                     break;
                 case "종료":
                     break outer;
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -23,9 +23,15 @@ public class AppTest {
         assertTrue(rs.contains("명령) "));
     }
     @Test
-    void  명령_등록_입력시_명언과_작가를_묻는지_Test() {
-        String rs = AppTestRunner.run("등록");
+    void  명령_등록_입력시_명언과_작가를_입력받는지_Test() {
+        String rs = AppTestRunner.run("""
+        등록
+        과거를 사랑하라
+        작자미상
+        종료
+        """);
 
+        System.out.print(rs);
         assertTrue(rs.contains("명언 : "));
         assertTrue(rs.contains("작가 : "));
     }
@@ -53,6 +59,23 @@ public class AppTest {
                 """);
         assertTrue(rs.contains("1번 명언이 등록되었습니다."));
         assertTrue(rs.contains("2번 명언이 등록되었습니다."));
+    }
+    @Test
+    void 명령어_목록_입력시_등록된_명언들을_출력하는지() {
+        String rs = AppTestRunner.run("""
+                등록
+                과거를 사랑하라
+                작자미상
+                등록
+                현재를 사랑하라
+                작자모름
+                목록
+                종료
+                """);
+        System.out.print(rs);
+        assertTrue(rs.contains("번호 / 작가 / 명언"));
+        assertTrue(rs.contains("2 / 작자모름 / 현재를 사랑하라"));
+        assertTrue(rs.contains("1 / 작자미상 / 과거를 사랑하라"));
     }
 
 }
